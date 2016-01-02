8ed778f8b5ef5f9fd8a1230b902b1cc457fd0964
n-a-m-e <lutz.steinel@gmail.com>
2016-01-02 19:00:56 +1100

    Create FacebookMessages.js

---
+++ FacebookMessages.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        facebook
+// @namespace   n-a-m-e
+// @include     https://m.facebook.com/messages/read/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+(function(){
+  var css = '#composerInput{height:400px !important;width:300px !important;margin:42px 0 0 -89px !important;}',
+      head = document.head || document.getElementsByTagName('head')[0],
+      style = document.createElement('style');
+
+  style.type = 'text/css';
+  if (style.styleSheet){
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  head.appendChild(style);
+})();
